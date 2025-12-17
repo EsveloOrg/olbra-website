db912a0a66fe45ab895e886519e256c818f8779e
Kacperek <kacper.kordus@esvelo.com>
2025-12-17 17:39:09 +0100

    footer and navbar fix

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,15 @@
 import { defineConfig } from 'vite'
 import { resolve } from 'path'
+import handlebars from 'vite-plugin-handlebars'
 
 export default defineConfig({
   root: '.',
   publicDir: 'assets',
+  plugins: [
+    handlebars({
+      partialDirectory: resolve(__dirname, 'src/partials'),
+    }),
+  ],
   build: {
     outDir: 'dist',
     rollupOptions: {
